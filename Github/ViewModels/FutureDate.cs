982718f29076565149735b3d42530ec5f017a6fa
nhatran03 <tranthanhnha84@gmail.com>
2018-05-14 22:38:30 -0500

    add button and include DB

--- Github/ViewModels/FutureDate.cs
+++ Github/ViewModels/FutureDate.cs
@@ -4,17 +4,17 @@ using System.Globalization;
 
 namespace Github.ViewModels
 {
-    public class FutureDate: ValidationAttribute
+	public class FutureDate: ValidationAttribute
     {
         public override bool IsValid(object value)
         {
-            DateTime datetime;
+            DateTime dateTime;
             var isValid = DateTime.TryParseExact(Convert.ToString(value), 
                 "d MMM YYYY", 
                 CultureInfo.CurrentCulture, 
                 DateTimeStyles.None, 
-                out datetime);
-            return (isValid && datetime > DateTime.Now);
+                out dateTime);
+            return (isValid && dateTime > DateTime.Now);
             
         }
     }
